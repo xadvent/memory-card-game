cc0545cd83c51f51ca55ba71a4d8a8eb0506b4ab
xadvent <piercestrutt@gmail.com>
2024-05-14 23:51:29 -0600

    feat: add dynamic card css
    Added dynamic CSS for the cards - allowing dynamic sizing.
    IMG's changed to use official artwork rather than whatever it was previously.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import propTypes from 'prop-types';
 
 async function apiCall(id) {
     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
@@ -6,14 +7,36 @@ async function apiCall(id) {
     return data;
 }
 
+const imgStyle = {
+    width: 100 + "px",
+}
+
+const cardStyle = {
+    border: "1px solid grey",
+    "border-radius": "20px",
+    height: "200px",
+    cursor: "pointer",
+    "-webkit-box-shadow": "0px 10px 28px -15px rgba(0,0,0,0.75)",
+    "-moz-box-shadow": "0px 10px 28px -15px rgba(0,0,0,0.75)",
+    "box-shadow": "0px 10px 28px -15px rgba(0,0,0,0.75)",
+}
+
+
+const cardNameStyle = {
+    "margin-top": 30 + "px",
+    "font-weight": "bold",
+    "letter-spacing": 1.5 + "px",
+}
+
 async function getPokemon(idArr) {
     const pokemonCards = await Promise.all(
         idArr.map(async (id, index) => {
             const response = await apiCall(id);
             return (
-                <div className="card" key={index}>
-                    <img src={response.sprites.front_default} alt={response.name} />
-                    <div className="title">{response.name}</div>
+                <div className="card" key={index} style={cardStyle} >
+                    <img src={response.sprites.other["official-artwork"].front_default} alt={response.name}
+                        style={imgStyle} />
+                    <div className="title" style={cardNameStyle}>{response.name.charAt(0).toUpperCase() + response.name.substring(1)}</div>
                 </div>
             );
         })
@@ -43,6 +66,10 @@ function Card({ characterList }) {
     }, [characterList]);
 
     return <>{pokemonCards}</>;
+}
+
+Card.propTypes = {
+    characterList: propTypes.array
 }
 
 export default Card;
